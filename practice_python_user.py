6da59e74d8a4df5d9f4c41044150da64a30a0479
renatogm24 <renato.garay@pucp.pe>
2022-01-17 17:09:09 -0500

    practice_python_chaining_methods

--- practice_python_user.py
+++ practice_python_user.py
@@ -6,47 +6,34 @@ class Usuario:		# esto es lo que tenemos hasta ahora
     # agregando el método de depósito
     def hacer_deposito(self, amount):
         self.balance_cuenta += amount
+        return self
     def hacer_retiro(self, amount):
         self.balance_cuenta -= amount
+        return self
     def mostrar_balance_usuario(self):
         print(f"Usuario: {self.name}, Balance:${self.balance_cuenta}")
+        return self
     def transfer_dinero(self, other_user, amount):
         self.hacer_retiro(amount)
         other_user.hacer_deposito(amount)
+        return self
 
 user1 = Usuario("Renato","abc@test.com")
 user2 = Usuario("Paco","abc@test.com")
 user3 = Usuario("Vanessa","abc@test.com")
 
-user1.hacer_deposito(500)
-user1.hacer_deposito(300)
-user1.hacer_deposito(200)
-user1.hacer_retiro(400)
-
-user1.mostrar_balance_usuario()
+user1.hacer_deposito(500).hacer_deposito(300).hacer_deposito(200).hacer_retiro(400).mostrar_balance_usuario()
 user2.mostrar_balance_usuario()
 user3.mostrar_balance_usuario()
 
-user2.hacer_deposito(500)
-user2.hacer_deposito(300)
-user2.hacer_retiro(200)
-user2.hacer_retiro(400)
+user1.mostrar_balance_usuario()
+user2.hacer_deposito(500).hacer_deposito(300).hacer_retiro(200).hacer_retiro(400).mostrar_balance_usuario()
+user3.mostrar_balance_usuario()
 
 user1.mostrar_balance_usuario()
 user2.mostrar_balance_usuario()
-user3.mostrar_balance_usuario()
+user3.hacer_deposito(1500).hacer_retiro(300).hacer_retiro(200).hacer_retiro(400).mostrar_balance_usuario()
 
-user3.hacer_deposito(1500)
-user3.hacer_retiro(300)
-user3.hacer_retiro(200)
-user3.hacer_retiro(400)
-
-user1.mostrar_balance_usuario()
-user2.mostrar_balance_usuario()
-user3.mostrar_balance_usuario()
-
-user1.transfer_dinero(user3,200)
-
-user1.mostrar_balance_usuario()
+user1.transfer_dinero(user3,200).mostrar_balance_usuario()
 user2.mostrar_balance_usuario()
 user3.mostrar_balance_usuario()
